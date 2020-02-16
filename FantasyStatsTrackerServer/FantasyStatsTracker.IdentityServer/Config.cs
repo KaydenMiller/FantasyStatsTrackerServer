25a24473372bef7ab420570f3b931cc815eb8bbf
Kayden Miller <kaydenmiller1@gmail.com>
2020-02-16 04:26:00 -0700

    Update project for Identity Server

---
+++ FantasyStatsTrackerServer/FantasyStatsTracker.IdentityServer/Config.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace FantasyStatsTracker.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> Apis => new List<ApiResource>
+        {
+            new ApiResource("characters", "Characters")
+        };
+        
+        public static IEnumerable<Client> Clients => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "FantasyStatsTrackerMobile",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("changeMe".Sha256())
+                },
+                AllowedScopes = { "characters" }
+            }
+        };
+    }
+}
--- FantasyStatsTrackerServer/FantasyStatsTracker.IdentityServer/Startup.cs
+++ FantasyStatsTrackerServer/FantasyStatsTracker.IdentityServer/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +11,11 @@ namespace FantasyStatsTracker.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = services.AddIdentityServer()
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients);
+
+            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +26,14 @@ namespace FantasyStatsTracker.IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseIdentityServer();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+            // app.UseRouting();
+
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            // });
         }
     }
 }
